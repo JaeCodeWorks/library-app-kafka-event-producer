bf78938f812face76779b30c26c8ddfb8ac1c2a6
shree.limbu <shree.limbu@gamesys.co.uk>
2023-10-04 22:27:06 +0100

    GTECH-1500: initial skeleton commit

---
+++ src/main/java/com/company/domain/LibraryEvent.java
@@ -0,0 +1,23 @@
+package com.company.domain;
+
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class LibraryEvent {
+
+    private Integer libraryEventId;
+    private LibraryEventType libraryEventType;
+
+    @NotNull
+    @Valid
+    private Book book;
+}
